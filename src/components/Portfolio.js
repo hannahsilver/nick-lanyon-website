b58ca6c0e0002a920b7799413dde58f4e929ce82
Hannah Silver <hannah_silver@hotmail.com>
2023-01-11 09:57:26 -0500

    fix

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,40 +1,108 @@
 import React from "react";
 
 function Portfolio() {
-
-    return(
-        <div className="portfolio">
-            <div className="section">
-                <div className="section-header">
-                    <p className="title">“Evergreen” (Short Film Score, 2022) </p>
-                    <p>Score composer, recording engineer </p>
-                </div>
-                <div className="media-container"> 
-                    <iframe className="bandcamp" height="360px" title="bandcamp-player" frameBorder="0" src="https://bandcamp.com/EmbeddedPlayer/album=3396749703/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="https://nicklanyon.bandcamp.com/album/evergreen-score">Evergreen Score by Nick Lanyon</a></iframe>
-                    <iframe className="vimeo"  title="vimeo-player" src="https://player.vimeo.com/video/711412436?h=1ba67a7e34" frameBorder="0" allowFullScreen></iframe>
-                </div>            
-            </div>
-            <div className="section">
-                <div className="section-header">
-                    <p className="title">Rapallo, <span>You Were Born to Drive This Car</span> (EP, 2022)</p>
-                    <p>Songwriter and arranger, producer, recording engineer, performer (guitar, bass, vocals, synthesizer)</p>
-                </div>
-                <div className="media-container">
-                    <iframe className="bandcamp" height="270px" title="bancamp-player" frameBorder="0"   src="https://bandcamp.com/EmbeddedPlayer/album=3451218775/size=large/bgcol=ffffff/linkcol=333333/artwork=small/transparent=true/" seamless><a href="https://rapallo1.bandcamp.com/album/you-were-born-to-drive-this-car">You Were Born to Drive This Car by Rapallo</a></iframe>
-                </div>
-            </div>
-            <div className="section">
-                <div className="section-header">
-                    <p className="title">Rapallo, “Fine Living” (Single, 2020 via <span>Nice Guys Records</span>)</p>
-                    <p>Songwriter and arranger, producer, recording engineer, performer (guitar, bass, vocals, drum programming, synthesizer)</p>
-                </div>
-                <div className="media-container">
-                    <iframe className="bandcamp" height="120px" title="bancamp-player" frameBorder="0"  src="https://bandcamp.com/EmbeddedPlayer/track=41079262/size=large/bgcol=ffffff/linkcol=7137dc/tracklist=false/artwork=small/transparent=true/" seamless><a href="https://niceguysweare.bandcamp.com/track/fine-living">Fine Living by Rapallo</a></iframe>
-                </div>
-            </div>
+  return (
+    <div className="portfolio">
+      <div className="section">
+        <div className="section-header">
+          <p className="title">Album title here...</p>
+          <p className="album-info">Album info here....</p>
         </div>
-    )
+        <div className="media-container">
+          <iframe
+            className="bandcamp"
+            height="360px"
+            title="bandcamp-player"
+            frameBorder="0"
+            src="https://bandcamp.com/EmbeddedPlayer/album=3396749703/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/"
+            seamless
+          >
+            <a href="https://nicklanyon.bandcamp.com/album/evergreen-score">
+              Evergreen Score by Nick Lanyon
+            </a>
+          </iframe>
+        </div>
+      </div>
+      <div className="section">
+        <div className="section-header">
+          <p className="title">“Evergreen” (Short Film Score, 2022) </p>
+          <p className="album-info">Score composer, recording engineer </p>
+        </div>
+        <div className="media-container">
+          <iframe
+            className="bandcamp"
+            height="360px"
+            title="bandcamp-player"
+            frameBorder="0"
+            src="https://bandcamp.com/EmbeddedPlayer/album=3396749703/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/"
+            seamless
+          >
+            <a href="https://nicklanyon.bandcamp.com/album/evergreen-score">
+              Evergreen Score by Nick Lanyon
+            </a>
+          </iframe>
+          <iframe
+            className="vimeo"
+            title="vimeo-player"
+            src="https://player.vimeo.com/video/711412436?h=1ba67a7e34"
+            frameBorder="0"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+      <div className="section">
+        <div className="section-header">
+          <p className="title">
+            Rapallo, <span>You Were Born to Drive This Car</span> (EP, 2022)
+          </p>
+          <p className="album-info">
+            Songwriter and arranger, producer, recording engineer, performer
+            (guitar, bass, vocals, synthesizer)
+          </p>
+        </div>
+        <div className="media-container">
+          <iframe
+            className="bandcamp"
+            height="270px"
+            title="bancamp-player"
+            frameBorder="0"
+            src="https://bandcamp.com/EmbeddedPlayer/album=3451218775/size=large/bgcol=ffffff/linkcol=333333/artwork=small/transparent=true/"
+            seamless
+          >
+            <a href="https://rapallo1.bandcamp.com/album/you-were-born-to-drive-this-car">
+              You Were Born to Drive This Car by Rapallo
+            </a>
+          </iframe>
+        </div>
+      </div>
+      <div className="section">
+        <div className="section-header">
+          <p className="title">
+            Rapallo, “Fine Living” (Single, 2020 via{" "}
+            <span>Nice Guys Records</span>)
+          </p>
+          <p className="album-info">
+            Songwriter and arranger, producer, recording engineer, performer
+            (guitar, bass, vocals, drum programming, synthesizer)
+          </p>
+        </div>
+        <div className="media-container">
+          <iframe
+            className="bandcamp"
+            height="120px"
+            title="bancamp-player"
+            frameBorder="0"
+            src="https://bandcamp.com/EmbeddedPlayer/track=41079262/size=large/bgcol=ffffff/linkcol=7137dc/tracklist=false/artwork=small/transparent=true/"
+            seamless
+          >
+            <a href="https://niceguysweare.bandcamp.com/track/fine-living">
+              Fine Living by Rapallo
+            </a>
+          </iframe>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Portfolio
-
+export default Portfolio;
--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,43 +1,54 @@
-import React, {useState} from "react"
-import {NavLink, Link} from 'react-router-dom'
-import Button from './Button'
-import './Navbar.css' 
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import Button from "./Button";
+import "./Navbar.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function Navbar() {
-    const [click, setClick] = useState(false)
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    return (
-        <div>
-            <nav className="navbar">
-                <div className="navbar-top">
-                    <Link to="nicklanyon/" className="navbar-logo">Nick Lanyon</Link>
-                    <div className="menu-icon" onClick={handleClick}>
-                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
-                    </div>
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className="nav-item">
-                        <NavLink to="nicklanyon/" className="nav-links" activeclassname="active" onClick={closeMobileMenu}>
-                            portfolio
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="nicklanyon/about" className="nav-links" activeclassname="active" onClick={closeMobileMenu}>
-                            about
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <Button mailTo="mailTo:nicklanyon93@gmail.com"/>
-                    </li>
-                </ul>
-            </nav>
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="navbar-top">
+          <Link to="nicklanyon/" className="navbar-logo">
+            Nick Lanyon
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
         </div>
-    )
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              to="nicklanyon/"
+              className="nav-links"
+              activeclassname="active"
+              onClick={closeMobileMenu}
+            >
+              Portfolio
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="nicklanyon/about"
+              className="nav-links"
+              activeclassname="active"
+              onClick={closeMobileMenu}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <Button mailTo="mailTo:nicklanyon93@gmail.com" />
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
